f77dc65a3a8b62737f5b5bb3e00328da7e964844
John Duavis <paulduavis09@gmail.com>
2025-02-21 00:34:09 +0800

    Configure database connection

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -1,35 +1,60 @@
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.EntityFrameworkCore;
-// using TodoApi.Data;
-// using TodoApi.Models;
-//
-// namespace TodoApi.Controllers;
-//
-// public class TodoController
-// {
-//     [Route("api/todos")]
-//     [ApiController]
-//     public class TodoController : ControllerBase
-//     {
-//         private readonly TodoDbContext _context;
-//
-//         public TodoController(TodoDbContext context)
-//         {
-//             _context = context;
-//         }
-//
-//         [HttpGet]
-//         public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodos()
-//         {
-//             return await _context.TodoItems.ToListAsync();
-//         }
-//         
-//         [HttpGet("id")]
-//         public async Task<ActionResult<TodoItem>> GetTodayBy(int id)
-//         {
-//             var todo = await _context.TodoItems.FindAsync(id);
-//             if (todo == null) return NotFound();
-//             return todo;
-//         }
-//     }
-// }
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Data;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers;
+
+    [Route("api/todos")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoDbContext _context;
+
+        public TodoController(TodoDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodos()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoById(int id)
+        {
+            var todo = await _context.TodoItems.FindAsync(id);
+            if (todo == null) return NotFound();
+            return todo;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> AddTodo(TodoItem todo)
+        {
+            _context.TodoItems.Add(todo);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetTodoById), new { id = todo.Id }, todo);
+        }
+
+        
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTodo(int id, TodoItem todo)
+        {
+            if (id != todo.Id) return BadRequest();
+            _context.Entry(todo).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodo(int id)
+        {
+            var todo = await _context.TodoItems.FindAsync(id);
+            if (todo == null) return NotFound();
+            _context.TodoItems.Remove(todo);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf9887ad800f417ee0b199356b028b7d9dd4667d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
