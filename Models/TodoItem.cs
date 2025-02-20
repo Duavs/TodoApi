d51d8960ac17fe0256cd682c3db1901a934ae294
John Duavis <paulduavis09@gmail.com>
2025-02-20 22:54:33 +0800

    Initial Commit API for Angular-TodoList

---
+++ Models/TodoItem.cs
@@ -0,0 +1,10 @@
+namespace TodoApi.Models;
+
+public class TodoItem
+{
+    public int Id { get; set; }
+    public string Task { get; set; } = string.Empty;
+    public bool Completed { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TodoDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
