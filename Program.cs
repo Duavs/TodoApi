ab16bcc56b2c7b10a6e49aae607db495e75d0fca
John Duavis <paulduavis09@gmail.com>
2025-02-23 10:13:47 +0800

    Able to communicate to the frontend

--- Program.cs
+++ Program.cs
@@ -3,8 +3,18 @@ using TodoApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configure Database Context
 builder.Services.AddDbContext<TodoDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Enable CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular",
+        policy => policy.WithOrigins("http://localhost:4200") // ✅ Allow Angular app
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -14,6 +24,10 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+
+// Use CORS before Authorization & Controllers
+app.UseCors("AllowAngular");
+
 app.UseAuthorization();
 app.MapControllers();
 
