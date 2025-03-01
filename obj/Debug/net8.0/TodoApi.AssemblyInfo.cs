079ff736f57f6eb0fc5d8894c4dd72ab4c98f4ff
John Duavis <paulduavis09@gmail.com>
2025-03-01 17:17:07 +0800

    Configure login API

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6dcac3c9aaf858bfa763e99312722a6bc23b2eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df243f9de5b6f4986b60d250773c64b61fb68090")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
