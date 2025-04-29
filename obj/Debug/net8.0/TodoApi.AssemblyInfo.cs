fc4bc951f9d7b532d5255493c293416136bed06d
John Duavis <paulduavis09@gmail.com>
2025-04-29 23:15:25 +0800

    Revert "changes"
    This reverts commit 884b31dae96794d3b8d205d4d285d3fa316856b2.

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68428cad3c6f078bb728c62938d4baee7e1ef46a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ca749b504b5997b20cfed47840cc29e65836270")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
