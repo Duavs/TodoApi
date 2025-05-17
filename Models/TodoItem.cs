d7fe5432da5143e411a6e8231a0d01f6d004e576
John Duavis <paulduavis09@gmail.com>
2025-05-17 22:08:33 +0800

    Added start and due date for the task

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -8,6 +8,8 @@ public class TodoItem
     public string Task { get; set; } = string.Empty;
     public string TaskDetail { get; set; } = string.Empty;
     public bool Completed { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime DueDate { get; set; } 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public bool IsDeleted { get; set; } = false;
     [Required]
