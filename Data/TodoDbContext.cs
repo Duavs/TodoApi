b1ebc737ef53e9916431a4386398b56533657448
John Duavis <paulduavis09@gmail.com>
2025-05-28 00:18:51 +0800

    Add DbSet properties for TaskSeverity, TaskStatus, and TaskType in TodoDbContext

--- Data/TodoDbContext.cs
+++ Data/TodoDbContext.cs
@@ -8,5 +8,8 @@ public class TodoDbContext : DbContext
     public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options) { }
     public DbSet<TodoItem> TodoItems { get; set; }
     public DbSet<User> Users { get; set; }
+    public DbSet<FM_TaskSeverity> FM_TaskSeverities { get; set; }
+    public DbSet<FM_TaskStatus> FM_TaskStatuses { get; set; }
+    public DbSet<FM_TaskType> FM_TaskTypes { get; set; }
 
 }
