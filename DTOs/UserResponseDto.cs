3767b2ea0a8bf3f5b69b3eb63fbe335863ed7e66
John Duavis <paulduavis09@gmail.com>
2025-05-29 22:21:03 +0800

    Update UserResponseDto properties to use required keyword for improved validation

--- DTOs/UserResponseDto.cs
+++ DTOs/UserResponseDto.cs
@@ -6,9 +6,9 @@ public class UserResponseDto
 {
     public int Id { get; set; }
     
-    public string Username { get; set; }
+    public required string Username { get; set; }
 
-    public string Email { get; set; }
+    public required string Email { get; set; }
 
     public string Role { get; set; }
 
