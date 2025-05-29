6c9548dd6dae4f70051a262187419d59bd0fbead
John Duavis <paulduavis09@gmail.com>
2025-05-29 22:20:26 +0800

    Update UserRegisterDto properties to use required keyword for improved validation

--- DTOs/UserRegisterDto.cs
+++ DTOs/UserRegisterDto.cs
@@ -4,20 +4,20 @@ namespace TodoApi.DTOs;
 
 public class UserRegisterDto
 {
-    [Required] public string Username { get; set; }
+    [Required] public required string Username { get; set; }
 
     [Required] 
     [EmailAddress] 
-    public string Email { get; set; }
+    public required string Email { get; set; }
 
     [Required] 
-    public string Password { get; set; }
+    public required string Password { get; set; }
 
     public bool Status { get; set; } // Renamed from isActive
     
-    [Required] public string FirstName { get; set; }
+    [Required] public required string FirstName { get; set; }
 
-    [Required] public string LastName { get; set; }
+    [Required] public required string LastName { get; set; }
 
     [Required] public string Address { get; set; }
 }
