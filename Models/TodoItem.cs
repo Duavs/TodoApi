bea3bf0704acb18397a7fe69908218924d318b9a
John Duavis <paulduavis09@gmail.com>
2025-05-30 21:15:00 +0800

    Update TodoItem properties to use required keyword for improved validation

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -5,16 +5,15 @@ namespace TodoApi.Models;
 public class TodoItem
 {
     public int Id { get; set; }
-    public string Task { get; set; } = string.Empty;
-    public string TaskDetail { get; set; } = string.Empty;
+    public required string Task { get; set; } = string.Empty;
+    public required string TaskDetail { get; set; } = string.Empty;
     public bool Completed { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; } 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public bool IsDeleted { get; set; } = false;
-    [Required]
-    public int UserId { get; set; }
-    public int TaskTypeId { get; set; }
-    public int TaskSeverityId { get; set; }
-    public int TaskStatusId { get; set; }
+    public required int UserId { get; set; }
+    public required int TaskTypeId { get; set; }
+    public required int TaskSeverityId { get; set; }
+    public required int TaskStatusId { get; set; }
 }
